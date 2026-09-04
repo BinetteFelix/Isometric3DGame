7a4ab978b4534d98c1f9236d60f71fba28165254
BinetteFelix <153897486+BinetteFelix@users.noreply.github.com>
2026-09-04 15:49:30 +0200

    Fixed an issue with faulty assembly file
    assembly file was gone and intellisense wasn't working
    
    some scriptable object for uivariables

---
+++ AGENTS.md
@@ -0,0 +1,4 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: Isometric3DGame
+- Unity version: Unity 6000.5.1f1
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/Scripts/ScriptableObject/UIVariables.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UIVariables", menuName = "ScriptableObjects/UIVariables", order = 0)]
+public class UIVariables : ScriptableObject
+{
+    //[Header("Pause Menu")]
+  
+    //[Space(10)]
+    
+
+    //[Space(25)]
+
+    //[Header("Main Menu")]
+    
+
+
+}
