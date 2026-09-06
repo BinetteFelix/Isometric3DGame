b3b1d898650dc188d9381df870610b996ddf7cf4
BinetteFelix <felix.binette@elev.ga.lbs.se>
2026-09-06 23:38:51 +0200

    Made a simple animation for pausing the game
    A simple animation and trying to learn unitys new input system, its not going so well...

--- Assets/Scripts/UI/SceneController.cs
+++ Assets/Scripts/UI/SceneController.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SceneController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static SceneController Instance;
+    [SerializeField] private InputAction PauseAction;
+
+    
+    [SerializeField] private Animator pauseAnimator;
+
+    [SerializeField] private GameObject pauseMenu;
+
+    public bool IsPaused { get; private set; }
+
+    private void Start()
+    {
+     
+    }
+    private void Update()
     {
         
+
+        #region INPUT HANDLER
+        if (PauseAction.IsPressed())
+        {
+            Debug.Log("AGNAGN");
+            Pause();
+        }
+        #endregion
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void Pause()
     {
-        
+        IsPaused = !IsPaused;
+
+        pauseAnimator.SetBool("PauseState", IsPaused);
+        pauseAnimator.SetTrigger("PauseInput");
     }
+    
 }
