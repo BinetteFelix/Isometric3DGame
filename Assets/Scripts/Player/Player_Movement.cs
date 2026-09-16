5531cf7ca9e0abb6c14c9b206e9a180b9cd61446
BinetteFelix <153897486+BinetteFelix@users.noreply.github.com>
2026-09-16 12:43:27 +0200

    Unused assets, Player rotation-walking direction animation
    Removed a bunch of unused assets, for some reason gave a major performance boost.
    
    Fixed the problem with the player rotating and still walking as if it hadn't rotated. (not very optimized code but it works)

--- Assets/Scripts/Player/Player_Movement.cs
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -11,9 +11,10 @@ public class Player_Movement : MonoBehaviour
     [SerializeField] public InputAction movementAction;
 
     private Vector3 _moveInput;
+    private Vector2 WalkDirection;
     private int AngleOffset = 45;
     
-    private float _moveSpeed = 5;
+    private float _moveSpeed = 3;
     private float _animatingMoveSpeed = 1;
 
     private void Awake()
@@ -32,20 +33,125 @@ public class Player_Movement : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
-        Debug.Log(_moveInput);
     }
     void GatherInputs()
     {
         _moveInput = new Vector3(movementAction.ReadValue<Vector2>().x, 0, movementAction.ReadValue<Vector2>().y);
-        p_Animator.SetFloat("WalkDirX", _moveInput.x);
-        p_Animator.SetFloat("WalkDirY", _moveInput.z);
-        p_Animator.SetFloat("TraversingSpeed", _animatingMoveSpeed);
+        
     }
     private float CalculateLookDirection()
     {
         Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
         Vector2 mouseOnScreen = Camera.main.ScreenToViewportPoint(Mouse.current.position.ReadValue());
         float angle = -AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen) - AngleOffset;
+
+        #region Walk Animation Direction
+
+        #region Forward
+        // Walk Forward Conditions
+        if (mouseOnScreen.x > 0.5f && (mouseOnScreen.y > 0.25f && mouseOnScreen.y < 0.75f) && _moveInput.x > 0)
+            WalkDirection = new Vector2(0, 1);
+        else if (mouseOnScreen.x < 0.5f && (mouseOnScreen.y > 0.25f && mouseOnScreen.y < 0.75f) && _moveInput.x < 0)
+            WalkDirection = new Vector2(0, 1);
+        else if (mouseOnScreen.y > 0.5f && (mouseOnScreen.x > 0.25f && mouseOnScreen.x < 0.75f) && _moveInput.z > 0)
+            WalkDirection = new Vector2(0, 1);
+        else if (mouseOnScreen.y < 0.5f && (mouseOnScreen.x > 0.25f && mouseOnScreen.x < 0.75f) && _moveInput.z < 0)
+            WalkDirection = new Vector2(0, 1);
+
+        // Walk Forward in Corner
+        else if (mouseOnScreen.y > 0.5f && (mouseOnScreen.x > 0.75f) && (_moveInput.x > 0 || _moveInput.z > 0) && !(_moveInput.x < 0 || _moveInput.z < 0))
+            WalkDirection = new Vector2(0, 1);
+        else if (mouseOnScreen.y < 0.5f && (mouseOnScreen.x > 0.75f) && (_moveInput.x > 0 || _moveInput.z < 0) && !(_moveInput.x < 0 || _moveInput.z > 0))
+            WalkDirection = new Vector2(0, 1);
+        else if (mouseOnScreen.y > 0.5f && (mouseOnScreen.x < 0.25f) && (_moveInput.x < 0 || _moveInput.z > 0) && !(_moveInput.x > 0 || _moveInput.z < 0))
+            WalkDirection = new Vector2(0, 1);
+        else if (mouseOnScreen.y < 0.5f && (mouseOnScreen.x < 0.25f) && (_moveInput.x < 0 || _moveInput.z < 0) && !(_moveInput.x > 0 || _moveInput.z > 0))
+            WalkDirection = new Vector2(0, 1);
+        #endregion
+
+        #region Backward
+        // Walk Backward Conditions
+        else if (mouseOnScreen.x > 0.5f && (mouseOnScreen.y > 0.25f && mouseOnScreen.y < 0.75f) && _moveInput.x < 0)
+            WalkDirection = new Vector2(0, -1);
+        else if (mouseOnScreen.x < 0.5f && (mouseOnScreen.y > 0.25f && mouseOnScreen.y < 0.75f) && _moveInput.x > 0)
+            WalkDirection = new Vector2(0, -1);
+        else if (mouseOnScreen.y > 0.5f && (mouseOnScreen.x > 0.25f && mouseOnScreen.x < 0.75f) && _moveInput.z < 0)
+            WalkDirection = new Vector2(0, -1);
+        else if (mouseOnScreen.y < 0.5f && (mouseOnScreen.x > 0.25f && mouseOnScreen.x < 0.75f) && _moveInput.z > 0)
+            WalkDirection = new Vector2(0, -1);
+
+        // Walk Backward in Corner
+        else if (mouseOnScreen.y > 0.5f && (mouseOnScreen.x > 0.75f) && (_moveInput.x < 0 || _moveInput.z < 0) && !(_moveInput.x > 0 || _moveInput.z > 0))
+            WalkDirection = new Vector2(0, -1);
+        else if (mouseOnScreen.y < 0.5f && (mouseOnScreen.x > 0.75f) && (_moveInput.x < 0 || _moveInput.z > 0) && !(_moveInput.x > 0 || _moveInput.z < 0))
+            WalkDirection = new Vector2(0, -1);
+        else if (mouseOnScreen.y > 0.5f && (mouseOnScreen.x < 0.25f) && (_moveInput.x > 0 || _moveInput.z < 0) && !(_moveInput.x < 0 || _moveInput.z > 0))
+            WalkDirection = new Vector2(0, -1);
+        else if (mouseOnScreen.y < 0.5f && (mouseOnScreen.x < 0.25f) && (_moveInput.x > 0 || _moveInput.z > 0) && !(_moveInput.x < 0 || _moveInput.z < 0))
+            WalkDirection = new Vector2(0, -1);
+        #endregion
+
+        #region Left
+        // Walk Left Conditions
+        else if (mouseOnScreen.x > 0.5f && (mouseOnScreen.y > 0.25f && mouseOnScreen.y < 0.75f) && _moveInput.z > 0)
+            WalkDirection = new Vector2(-1, 0);
+        else if (mouseOnScreen.x < 0.5f && (mouseOnScreen.y > 0.25f && mouseOnScreen.y < 0.75f) && _moveInput.z < 0)
+            WalkDirection = new Vector2(-1, 0);
+        else if (mouseOnScreen.y > 0.5f && (mouseOnScreen.x > 0.25f && mouseOnScreen.x < 0.75f) && _moveInput.x < 0)
+            WalkDirection = new Vector2(-1, 0);
+        else if (mouseOnScreen.y < 0.5f && (mouseOnScreen.x > 0.25f && mouseOnScreen.x < 0.75f) && _moveInput.x > 0)
+            WalkDirection = new Vector2(-1, 0);
+
+        // Walk Left in Corner
+        else if (mouseOnScreen.y > 0.5f && (mouseOnScreen.x > 0.75f) && (_moveInput.x < 0 && _moveInput.z > 0))
+            WalkDirection = new Vector2(-1, 0);
+        else if (mouseOnScreen.y < 0.5f && (mouseOnScreen.x > 0.75f) && (_moveInput.x > 0 && _moveInput.z > 0))
+            WalkDirection = new Vector2(-1, 0);
+        else if (mouseOnScreen.y > 0.5f && (mouseOnScreen.x < 0.25f) && (_moveInput.x < 0 || _moveInput.z < 0))
+            WalkDirection = new Vector2(-1, 0);
+        else if (mouseOnScreen.y < 0.5f && (mouseOnScreen.x < 0.25f) && (_moveInput.x > 0 || _moveInput.z < 0))
+            WalkDirection = new Vector2(-1, 0);
+        #endregion
+
+        #region Right
+        // Walk Right Conditions
+        else if (mouseOnScreen.x > 0.5f && (mouseOnScreen.y > 0.25f && mouseOnScreen.y < 0.75f) && _moveInput.z < 0)
+            WalkDirection = new Vector2(1, 0);
+        else if (mouseOnScreen.x < 0.5f && (mouseOnScreen.y > 0.25f && mouseOnScreen.y < 0.75f) && _moveInput.z > 0)
+            WalkDirection = new Vector2(1, 0);
+        else if (mouseOnScreen.y < 0.5f && (mouseOnScreen.x > 0.25f && mouseOnScreen.x < 0.75f) && _moveInput.x < 0)
+            WalkDirection = new Vector2(1, 0);
+        else if (mouseOnScreen.y > 0.5f && (mouseOnScreen.x > 0.25f && mouseOnScreen.x < 0.75f) && _moveInput.x > 0)
+            WalkDirection = new Vector2(1, 0);
+
+        // Walk Right in Corner
+        else if (mouseOnScreen.y > 0.5f && (mouseOnScreen.x > 0.75f) && (_moveInput.x > 0 && _moveInput.z < 0))
+            WalkDirection = new Vector2(1, 0);
+        else if (mouseOnScreen.y < 0.5f && (mouseOnScreen.x > 0.75f) && (_moveInput.x < 0 && _moveInput.z < 0))
+            WalkDirection = new Vector2(1, 0);
+        else if (mouseOnScreen.y > 0.5f && (mouseOnScreen.x < 0.25f) && (_moveInput.x > 0 || _moveInput.z > 0))
+            WalkDirection = new Vector2(1, 0);
+        else if (mouseOnScreen.y < 0.5f && (mouseOnScreen.x < 0.25f) && (_moveInput.x < 0 || _moveInput.z > 0))
+            WalkDirection = new Vector2(1, 0);
+        #endregion
+
+        #region Idle
+        // Idle Condition
+        else
+            WalkDirection = new Vector2(0, 0);
+        #endregion
+
+        p_Animator.SetFloat("WalkDirY", WalkDirection.y);
+        p_Animator.SetFloat("WalkDirX", WalkDirection.x);
+        p_Animator.SetFloat("TraversingSpeed", _animatingMoveSpeed);
+
+        if (p_Animator.GetFloat("WalkDirY") > 0)
+            _moveSpeed = 4;
+        else if (p_Animator.GetFloat("WalkDirY") < 0)
+            _moveSpeed = 3;
+        else if (p_Animator.GetFloat("WalkDirX") > 0 || p_Animator.GetFloat("WalkDirX") > 0)
+            _moveSpeed = 3.5f;
+        #endregion
 
         return angle;
     }
